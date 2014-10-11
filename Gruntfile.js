11bb88666d94bbe1f6ab396d20983bf5448bee1c
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-10-12 03:59:04 +0530

    updated for grunt file and deleted tmp files

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ module.exports = function (grunt) {
               loadPath: ['<%= config.app %>/sass']
             },
             files: {
-              '.tmp/styles/main.css': '<%= config.app %>/scss/bootstrap.scss',
+              '.tmp/styles/main.css': '<%= config.app %>/scss/_bootstrap.scss',
               '.tmp/styles/app.css': '<%= config.app %>/scss/demo.scss'
             }
           }
