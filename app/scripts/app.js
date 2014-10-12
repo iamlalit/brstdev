13be3ea28a8f91110d9a36fbe335e41825368696
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-10-13 01:41:24 +0530

    updated for footer and footer.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,9 @@
 'use strict';
 var EpageoApp = angular.module('EpageoApp', [])
 
-    .controller('Controller', ['$scope', function($scope) {
-    }]
-
-);
+.controller('Controller', ['$scope', function($scope) {
+	$scope.goToTop = function(){
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+	  		return false;
+	}
+}]);
