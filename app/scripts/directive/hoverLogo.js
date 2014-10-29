532d4efe7bf3810549255e82a23112107b5cbef7
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-10-30 02:03:10 +0530

    updated for hover logo

--- app/scripts/directive/hoverLogo.js
+++ app/scripts/directive/hoverLogo.js
@@ -2,11 +2,10 @@ EpageoApp.directive('hoverLogo', function() {
 	return {
 		restrict: 'A',
 		link: function (scope, elem, attrs, ctrl) {
-			var max = 4;
+			var max = 7;
 			var min = 1;
 			elem.bind('mouseover', function() {
-			  var flag = Math.floor(Math.random() * (max - min + 1)) + min;;
-			  console.log(flag);
+			  var flag = Math.floor(Math.random() * (max - min + 1)) + min;
 			  $(this).parent("a.navbar-brand").css('background-image', 'url("img/animated'+flag+'.gif")');  
 			});
 
