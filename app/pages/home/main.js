6f2dd561402e0edc769874aadc43a69701d231d0
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-12-18 11:48:09 +0530

    updtaed for home page to load dynamically

--- app/pages/home/main.js
+++ app/pages/home/main.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
       pause: ''
     });
     $('#contact-slider').off('keydown.bs.carousel');
+
+    $("#home-page")
+	    .load(function() { 
+	    	console.log("image loaded correctly"); 
+	    	$('body.index').css({'background': 'url("../../.tmp/img/Homepage_MainImage_rev1.gif")', 'background-size': 'cover'});
+	    })
+	    .error(function() { 
+	    	console.log("error loading image"); 
+	    });
 });
