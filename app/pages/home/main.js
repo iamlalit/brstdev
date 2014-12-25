1343a465e62029f1ee549a4aa6b3a28f65b902f7
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-12-25 17:03:39 +0530

    updated for fonts and adding new ones and updated for logo to have white logo and updated the carouse to move as it is desired to move

--- app/pages/home/main.js
+++ app/pages/home/main.js
@@ -16,81 +16,114 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	var kids = $('.magic-icons').children();
-	function animateIcons(){
-		$(kids[0]).delay(3000).fadeOut(1000, function() { 
-			$(kids[1]).css('display', 'inline-block');  
-			$(kids[1]).delay(3000).fadeOut(1000, function() {
-				$(kids[2]).css('display', 'inline-block');
-				$(kids[2]).delay(3000).fadeOut(1000, function() {
-					$(kids[0]).css('display', 'inline-block');
-					animateIcons();
-				})
-			}); 
-		});	
+	var kids = $('.ballon1 .magic-icons').children();
+	var iconList = ["icon-strategy", "icon-search", "icon-cog", "icon-location", "icon-tools", "icon-bucket", "icon-setting-alt", "icon-click", "icon-discovery", "icon-strategy", "icon-strategy"];	
+	var spanList = ["Lean Strategies", "Search Engine Optimization", "Landing Page Solution", "Local Search Optimization", "Website Development", "Successful Execution", "Content Creation", "Pay Per Click", "Web Analytics", "Lean Strategies"]
+	function animations(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList.length-1 ; j++){
+			$(kids[0]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids[0]).children('span').text(spanList[i+1])
+				$(kids[0]).css('display', 'inline-block').children('i').removeClass(iconList[i]).addClass(iconList[i+1]);	
+				if(i == 9){
+					animations();
+				}
+			});
+		};
 	}
-	function animateIcons1(){
-		$(kids[3]).delay(4000).fadeOut(1000, function() { 
-			$(kids[4]).css('display', 'inline-block');  
-			$(kids[4]).delay(4000).fadeOut(1000, function() {
-				$(kids[5]).css('display', 'inline-block');
-				$(kids[5]).delay(4000).fadeOut(1000, function() {
-					$(kids[3]).css('display', 'inline-block');
-					animateIcons1();
-				})
-			}); 
-		});	
-	}
+	animations();
 	
-
-	function animateIcons2(){
-		$(kids[6]).delay(6000).fadeOut(1000, function() { 
-			$(kids[7]).css('display', 'inline-block');  
-			$(kids[7]).delay(6000).fadeOut(1000, function() {
-				$(kids[8]).css('display', 'inline-block');
-				$(kids[8]).delay(6000).fadeOut(1000, function() {
-					$(kids[6]).css('display', 'inline-block');
-					animateIcons2();
-				})
-			}); 
-		});	
+	var iconList1 = ["icon-search", "icon-cog", "icon-location", "icon-tools", "icon-bucket", "icon-setting-alt", "icon-click", "icon-discovery", "icon-strategy", "icon-search", "icon-search"];	
+	var spanList1 = ["Search Engine Optimization", "Landing Page Solution", "Local Search Optimization", "Website Development", "Successful Execution", "Content Creation", "Pay Per Click", "Web Analytics", "Lean Strategies", "Search Engine Optimization"]
+	function animations1(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList1.length-1 ; j++){
+			$(kids[1]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids[1]).children('span').text(spanList1[i+1])
+				$(kids[1]).css('display', 'inline-block').children('i').removeClass(iconList1[i]).addClass(iconList1[i+1]);	
+				if(i == 9){
+					animations1();
+				}
+			});
+		};
 	}
+	animations1();
 	
-	// animateIcons();
-	// animateIcons1();
-	// animateIcons2();
+	var iconList2 = ["icon-cog", "icon-location", "icon-tools", "icon-bucket", "icon-setting-alt", "icon-click", "icon-discovery", "icon-strategy", "icon-search", "icon-cog", "icon-cog"];	
+	var spanList2 = ["Landing Page Solution", "Local Search Optimization", "Website Development", "Successful Execution", "Content Creation", "Pay Per Click", "Web Analytics", "Lean Strategies", "Search Engine Optimization", "Landing Page Solution"];
+	function animations2(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList2.length-1 ; j++){
+			$(kids[2]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids[2]).children('span').text(spanList2[i+1])
+				$(kids[2]).css('display', 'inline-block').children('i').removeClass(iconList2[i]).addClass(iconList2[i+1]);	
+				if(i == 9){
+					animations2();
+				}
+			});
+		};
+	}
+	animations2();
 
-	//ballon3
-	var kidds = $(".ballon3 .magic-icons").children();
-	function animateIcons3(){
-		$(kidds[0]).delay(3000).fadeOut(1000, function() { 
-			$(kidds[1]).css('display', 'inline-block');
-			$(kidds[1]).delay(3000).fadeOut(1000, function() {
-				$(kidds[0]).css('display', 'inline-block');
-				animateIcons3();
-			})
-		});	
+	//for ballon3
+	var kids2 = $('.ballon3 .magic-icons').children();
+	var iconList21 = ["icon-brain", "icon-crawler", "icon-bucket", "icon-network", "icon-beaker", "icon-algorithm", "icon-brain", "icon-brain"];	
+	var spanList21 = ["Business Intelligence", "Proprietary Crawlers", "Presentable Dashboards", "Analytics", "Research & Development", "Predective Modelling", "Business Intelligence", "Business Intelligence"];
+	function animations21(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList21.length-1 ; j++){
+			$(kids2[0]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids2[0]).children('span').text(spanList21[i+1])
+				$(kids2[0]).css('display', 'inline-block').children('i').removeClass(iconList21[i]).addClass(iconList21[i+1]);	
+				if(i == 6){
+					animations21();
+				}
+			});
+		};
 	}
-	function animateIcons4(){
-		$(kidds[2]).delay(4000).fadeOut(1000, function() { 
-			$(kidds[3]).css('display', 'inline-block');
-			$(kidds[3]).delay(4000).fadeOut(1000, function() {
-				$(kidds[2]).css('display', 'inline-block');
-				animateIcons4();
-			})
-		});	
+	animations21();
+	var iconList22 = ["icon-crawler", "icon-bucket", "icon-network", "icon-beaker", "icon-algorithm", "icon-brain", "icon-crawler", "icon-crawler"];	
+	var spanList22 = ["Proprietary Crawlers", "Presentable Dashboards", "Analytics", "Research & Development", "Predective Modelling", "Business Intelligence", "Proprietary Crawlers", "Proprietary Crawlers"];
+	function animations22(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList22.length-1 ; j++){
+			$(kids2[1]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids2[1]).children('span').text(spanList22[i+1])
+				$(kids2[1]).css('display', 'inline-block').children('i').removeClass(iconList22[i]).addClass(iconList22[i+1]);	
+				if(i == 6){
+					animations22();
+				}
+			});
+		};
 	}
-	function animateIcons5(){
-		$(kidds[4]).delay(5000).fadeOut(1000, function() { 
-			$(kidds[5]).css('display', 'inline-block');
-			$(kidds[5]).delay(5000).fadeOut(1000, function() {
-				$(kidds[4]).css('display', 'inline-block');
-				animateIcons5();
-			})
-		});	
+	animations22();
+	var iconList23 = ["icon-bucket", "icon-network", "icon-beaker", "icon-algorithm", "icon-brain", "icon-crawler", "icon-bucket", "icon-bucket"];	
+	var spanList23 = ["Presentable Dashboards", "Analytics", "Research & Development", "Predective Modelling", "Business Intelligence", "Proprietary Crawlers", "Presentable Dashboards", "Presentable Dashboards"];
+	function animations23(){
+		var i = -1;
+		var j;
+		for(j = 0; j< iconList23.length-1 ; j++){
+			$(kids2[2]).delay(4000).fadeOut(10, function() { 
+				i++;
+				$(kids2[2]).children('span').text(spanList23[i+1])
+				$(kids2[2]).css('display', 'inline-block').children('i').removeClass(iconList23[i]).addClass(iconList23[i+1]);	
+				if(i == 6){
+					animations23();
+				}
+			});
+		};
 	}
+	animations23();
 
-	// animateIcons3();
-	// animateIcons4();
-	// animateIcons5();
+	
 });
--- app/scripts/directive/hoverLogo.js
+++ app/scripts/directive/hoverLogo.js
@@ -8,10 +8,16 @@ EpageoApp.directive('hoverLogo', function() {
 			  var flag = Math.floor(Math.random() * (max - min + 1)) + min;
 			  $(this).closest("span.navbar-brand").css('background-image', 'url("../../.tmp/img/animated'+flag+'.gif")');  
 			});
-
 			elem.bind('mouseout', function(){
   				$(this).closest("span.navbar-brand").css('background-image', 'none');
-			})
+			});
+			scope.$watch('location.path()', function() {
+				if(window.location.pathname.split('/pages/')[1] == 'home/index.html'){
+					$(".navbar-brand").children('a').eq(1).css({'display': 'none'});
+				}else{
+					$(".navbar-brand").children('a').eq(0).css({'display': 'none'});
+				}
+			});
 		}
 	}    
 });
